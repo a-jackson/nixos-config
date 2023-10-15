b9ac0b532c80de24a5cdb6df47c89650f837c306
Andrew Jackson <andrew@a-jackson.co.uk>
2023-10-15 10:05:27 +0100

    add fish git options

--- modules/user.nix
+++ modules/user.nix
@@ -31,6 +31,10 @@
         config.enable = true;
         functions.enable = true;
       };
+      interactiveShellInit = "
+        set __fish_git_prompt_show_informative_status 1
+        set __fish_git_prompt_showupstream 1
+      ";
     };
   };
 
