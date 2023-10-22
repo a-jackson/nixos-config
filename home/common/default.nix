030bb0bd83888ecfe828033cafb3090af5feeaaa
Andrew Jackson <andrew@a-jackson.co.uk>
2023-10-22 09:28:03 +0100

    update xdg dirs

--- home/common/default.nix
+++ home/common/default.nix
@@ -61,6 +61,9 @@
       "repos"
       ".gnupg"
       ".ssh"
+      "documents"
+      "desktop"
+      "downloads"
     ];
   };
 
@@ -68,5 +71,18 @@
     username = lib.mkDefault "andrew";
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
     stateVersion = "23.05";
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    documents = "${config.home.homeDirectory}/documents";
+    desktop = "${config.home.homeDirectory}/desktop";
+    download = "${config.home.homeDirectory}/downloads";
+    music = null;
+    pictures = null;
+    videos = null;
+    templates = null;
+    publicShare = null;
   };
 }
