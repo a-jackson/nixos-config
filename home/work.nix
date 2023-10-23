c8d7c9b068c9f2d916bbec5ed2f1143a4c3a045f
Andrew Jackson <andrew@a-jackson.co.uk>
2023-10-23 10:34:40 +0100

    enable nix-command and flakes

--- home/work.nix
+++ home/work.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./common/shell.nix
   ];
+
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
 }
