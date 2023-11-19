2841142b9321a26ef37484d4e9e607bda104b51a
Andrew Jackson <andrew@a-jackson.co.uk>
2023-11-19 10:08:59 +0000

    add containers

--- server.nix
+++ server.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 let
   overlay-jellyseer = final: prev: {
     jellyseerr = final.callPackage ./packages/jellyseerr { };
@@ -53,6 +53,52 @@ in
   environment.persistence."/persist" = {
     directories = [
       "/var/lib/private/jellyseerr"
+      "/var/lib/sonarr"
+      "/var/lib/radarr"
     ];
+  };
+
+  systemd.services.init-media-network = {
+    description = "Create the network bridge for immich.";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+          # Put a true at the end to prevent getting non-zero return code, which will
+          # crash the whole service.
+          check=$(${pkgs.podman}/bin/podman network ls | grep "media" || true)
+          if [ -z "$check" ]; then
+            ${pkgs.podman}/bin/podman network create media
+          else
+               echo "media already exists in podman"
+           fi
+        '';
+  };
+
+  virtualisation.oci-containers.containers = {
+    sonarr = {
+      image = "lscr.io/linuxserver/sonarr:latest";
+      volumes = [
+        "/var/lib/sonarr:/config"
+      ];
+      ports = [
+        "8989:8989"
+      ];
+      extraOptions = [
+        "--network=media"
+      ];
+    };
+    radarr = {
+      image = "lscr.io/linuxserver/radarr:latest";
+      volumes = [
+        "/var/lib/radarr:/config"
+      ];
+      ports = [
+        "7878:7878"
+      ];
+      extraOptions = [
+        "--network=media"
+      ];
+    };
   };
 }
