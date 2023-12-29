8c17b591a8167ea82c510d296032dbaf3ab5fc49
Andrew Jackson <andrew@a-jackson.co.uk>
2023-12-29 08:26:11 +0000

    update hardward-configuration

--- hardware/apps.nix
+++ hardware/apps.nix
@@ -24,4 +24,7 @@
   }];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
