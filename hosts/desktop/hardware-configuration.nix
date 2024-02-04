374b32ea1f9501181f19368ccd8ac6238ed85bcf
Andrew Jackson <andrew@a-jackson.co.uk>
2024-02-04 13:11:23 +0000

    disable nvidia drivers

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -26,22 +26,4 @@
   hardware.enableRedistributableFirmware = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
- services.xserver.videoDrivers = [ "nvidia" ];
-
- hardware.nvidia = {
-   modesetting.enable = true;
-   powerManagement.enable = false;
-   powerManagement.finegrained = false;
-   open = true;
-   nvidiaSettings = true;
-   package = config.boot.kernelPackages.nvidiaPackages.stable;
- };
 }
