ad949d5c057ff8aeb84ea785182a86adb378c56b
Andrew Jackson <andrew@a-jackson.co.uk>
2024-02-26 20:17:32 +0000

    add packages to work

--- home/work.nix
+++ home/work.nix
@@ -14,6 +14,14 @@
   home.packages = with pkgs; [
     awscli2
     terraform
+    omnisharp-roslyn
+    tflint
+    packer
+    (with dotnetCorePackages; combinePackages [
+      sdk_6_0
+      sdk_7_0
+      sdk_8_0
+    ])
   ];
 
   programs = {
@@ -24,7 +32,7 @@
         signByDefault = true;
       };
       aliases = {
-        changelog = "!git log --format='* %s' --reverse --no-decorate --no-merges \${1-planned-release-dev}..HEAD";
+        changelog = "!git log --format='* %s' --reverse --no-decorate --no-merges \${1-planned-release-dev}..\${2-HEAD}";
         start = "!DEFAULT=$(git default) && git localtrim && git switch --no-track -c $1 origin/\${2-$DEFAULT} #";
         default = "!(git show-ref --verify --quiet refs/heads/planned-release-dev && echo planned-release-dev) || (git show-ref --verify --quiet refs/heads/master && echo master)";
         rd = "!DEFAULT=$(git default) && git checkout -B $DEFAULT origin/$DEFAULT";
@@ -44,6 +52,7 @@
           pr-devops = "!default_branch=`git default`;changelog=`git changelog origin/$default_branch`;title=`git head | sed \"s/-/ /2g\"`;gh pr create --title \"$title\" --body \"$changelog\" --reviewer adamwillden --reviewer markdfn --reviewer bryanpep --reviewer AltheusII";
           pr-dps = "!default_branch=`git default`;changelog=`git changelog origin/$default_branch`;title=`git head | sed \"s/-/ /2g\"`;gh pr create --title \"$title\" --body \"$changelog\" --reviewer martynguest --reviewer chrisjward67";
           pr-edit = "!default_branch=`git default`;changelog=`git changelog origin/$default_branch`;title=`git head | sed \"s/-/ /2g\"`;gh pr edit -b \"$changelog\" -t \"$title\"";
+          deploy-changes = "!env=$1;shift;sha=$(gh api \"repos/$(gh repo view --json nameWithOwner -q \".nameWithOwner\")/deployments?environment=$env\" -q \".[0].sha\");git changelog $sha $(git default) \"$@\"";
         };
       };
     };
