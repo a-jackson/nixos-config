2d4e97b81bd208d336497851136618c46ea8d292
Andrew Jackson <andrew@a-jackson.co.uk>
2024-03-02 21:40:45 +0000

    add samba for paperless consume

--- hosts/apps/paperless.nix
+++ hosts/apps/paperless.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ config, ... }: {
   services = {
     paperless = {
       enable = true;
@@ -9,6 +9,30 @@
         PAPERLESS_CONSUMER_RECURSIVE = "true";
       };
     };
+
+    samba = {
+      enable = true;
+      securityType = "user";
+      openFirewall = true;
+      extraConfig = ''
+        workgroup = WORKGROUP
+        server string = apps
+        netbios name = apps
+        hosts allow = 192.168.1.0/24 127.0.0.1 localhost
+        hosts deny 0.0.0.0/0
+      '';
+      shares = {
+        consume = {
+          path = config.services.paperless.consumptionDir;
+          browseable = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force user" = "paperless";
+          "force group" = "paperless";
+        };
+      };
+    };
   };
 }
-
