a8e2742dde2e169346fcc56df312e72c3cec37f8
Andrew Jackson <andrew@a-jackson.co.uk>
2024-03-15 09:00:39 +0000

    monitoring: add loki and promtail

--- hosts/apps/monitoring.nix
+++ hosts/apps/monitoring.nix
@@ -1,27 +1,85 @@
 { config, ... }: {
   homelab.monitoring.enable = true;
 
-  services.prometheus = {
-    enable = true;
+  services = {
+    prometheus = {
+      enable = true;
 
-    scrapeConfigs = [{
-      job_name = "apps";
-      static_configs = [{
-        targets = [
-          "127.0.0.1:${toString config.services.prometheus.exporters.smartctl.port}"
-          "127.0.0.1:${toString config.services.prometheus.exporters.node.port}"
-        ];
+      scrapeConfigs = [{
+        job_name = "apps";
+        static_configs = [{
+          targets = [
+            "127.0.0.1:${
+              toString config.services.prometheus.exporters.smartctl.port
+            }"
+            "127.0.0.1:${
+              toString config.services.prometheus.exporters.node.port
+            }"
+          ];
+        }];
       }];
-    }];
-  };
+    };
 
-  services.grafana = {
-    enable = true;
-    settings = {
-      server = {
-        http_addr = "127.0.0.1";
-        http_port = 3000;
-        domain = "grafana.ajackson.dev";
+    grafana = {
+      enable = true;
+      settings = {
+        server = {
+          http_addr = "127.0.0.1";
+          http_port = 3000;
+          domain = "grafana.ajackson.dev";
+        };
+      };
+    };
+
+    loki = {
+      enable = true;
+      configuration = {
+        auth_enabled = false;
+        server.http_listen_port = 3100;
+
+        ingester = {
+          lifecycler = {
+            address = "0.0.0.0";
+            ring.kvstore.store = "inmemory";
+            ring.replication_factor = 1;
+            final_sleep = "0s";
+          };
+          chunk_idle_period = "1h";
+          max_chunk_age = "1h";
+          chunk_target_size = 1048576;
+          chunk_retain_period = "30s";
+          max_transfer_retries = 0;
+        };
+
+        schema_config.configs = [{
+          from = "2020-10-24";
+          store = "boltdb-shipper";
+          object_store = "filesystem";
+          schema = "v11";
+          index.prefix = "index_";
+          index.period = "24h";
+        }];
+
+        compactor = {
+          working_directory = "/var/lib/loki/compactor";
+          shared_store = "filesystem";
+        };
+
+        storage_config.boltdb_shipper = {
+          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
+          cache_location = "/var/lib/loki/boltdb-shipper-cache";
+          cache_ttl = "24h";
+          shared_store = "filesystem";
+        };
+        storage_config.filesystem = { directory = "/var/lib/loki/chunks"; };
+
+        limits_config.reject_old_samples = true;
+        limits_config.reject_old_samples_max_age = "168h";
+
+        chunk_store_config.max_look_back_period = "0s";
+
+        table_manager.retention_deletes_enabled = false;
+        table_manager.retention_period = "0s";
       };
     };
   };
--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -1,22 +1,47 @@
-{ config, lib, ... }: with lib;
-let
-  cfg = config.homelab.monitoring;
-in
-{
-  options.homelab.monitoring = {
-    enable = mkEnableOption "Enable monitoring";
-  };
+{ config, lib, ... }:
+with lib;
+let cfg = config.homelab.monitoring;
+in {
+  options.homelab.monitoring = { enable = mkEnableOption "Enable monitoring"; };
 
   config = mkIf cfg.enable {
     services.prometheus.exporters = {
-      smartctl.enable = true;
-      smartctl.openFirewall = true;
-      node.enable = true;
-      node.openFirewall = true;
+      smartctl = {
+        enable = true;
+        openFirewall = true;
+      };
+
+      node = {
+        enable = true;
+        openFirewall = true;
+        enabledCollectors = [ "systemd" ];
+      };
     };
 
     services.udev.extraRules = ''
       SUBSYSTEM=="nvme", KERNEL=="nvme[0-9]*", GROUP="disk"
     '';
+
+    services.promtail = {
+      enable = true;
+      configuration = {
+        server.http_listen_port = 28183;
+        server.grpc_listen_port = 0;
+        positions.filename = "/tmp/positions.yml";
+        clients = [{ url = "http://apps:3100/loki/api/v1/push"; }];
+        scrape_configs = [{
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels.job = "systemd-journal";
+            labels.host = config.networking.hostName;
+          };
+          relabel_configs = [{
+            source_labels = [ "__journal__systemd_unit" ];
+            target_label = "unit";
+          }];
+        }];
+      };
+    };
   };
 }
