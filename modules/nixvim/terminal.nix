e09cd61cadc9411e467c5478b3db71f300f18b3b
Andrew Jackson <andrew@a-jackson.co.uk>
2024-03-15 09:58:10 +0000

    nixvim: C-t for toggle terminal

--- modules/nixvim/terminal.nix
+++ modules/nixvim/terminal.nix
@@ -3,20 +3,21 @@
     programs.nixvim = {
       plugins.toggleterm.enable = true;
 
-      keymaps =
-        let
-          map = key: action: desc: mode: {
-            inherit key action mode;
-            options.desc = desc;
-          };
-        in
-        [
-          (map "<leader>tf" "<Cmd>ToggleTerm direction=float<CR>" "[T]erminal [F]loat" "n")
-          (map "<leader>th" "<Cmd>ToggleTerm direction=horizontal size=10<CR>" "[T]erminal [H]orizontal" "n")
-          (map "<leader>tv" "<Cmd>ToggleTerm direction=vertical size=80<CR>" "[T]erminal [V]ertical" "n")
-          (map "<F7>" "<Cmd>ToggleTerm<CR>" "Toggle Terminal" ["n" "t"])
-          (map "<C-'>" "<Cmd>ToggleTerm<CR>" "Toggle Terminal" ["n" "t"])
-        ];
+      keymaps = let
+        map = key: action: desc: mode: {
+          inherit key action mode;
+          options.desc = desc;
+        };
+      in [
+        (map "<leader>tf" "<Cmd>ToggleTerm direction=float<CR>"
+          "[T]erminal [F]loat" "n")
+        (map "<leader>th" "<Cmd>ToggleTerm direction=horizontal size=10<CR>"
+          "[T]erminal [H]orizontal" "n")
+        (map "<leader>tv" "<Cmd>ToggleTerm direction=vertical size=80<CR>"
+          "[T]erminal [V]ertical" "n")
+        (map "<F7>" "<Cmd>ToggleTerm<CR>" "Toggle Terminal" [ "n" "t" ])
+        (map "<C-t>" "<Cmd>ToggleTerm<CR>" "Toggle Terminal" [ "n" "t" ])
+      ];
     };
   };
 }
