9ea9711dcdbc3cbeb78baa92f2d20bc49cc006a0
Andrew Jackson <andrew@a-jackson.co.uk>
2024-03-27 21:01:49 +0000

    fix: disable network manager wait service

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,5 +1,4 @@
-{ pkgs, nixpkgs, lib, config, ... }:
-{
+{ pkgs, nixpkgs, lib, config, ... }: {
   imports = [
     ./sops.nix
     ./user.nix
@@ -17,53 +16,50 @@
 
   config = {
     boot.loader = lib.mkIf config.homelab.systemd-boot {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
-
-  networking = {
-    useDHCP = nixpkgs.lib.mkDefault true;
-    firewall.enable = true;
-    networkmanager.enable = true;
-  };
-
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-  ];
-
-  services.tailscale.enable = true;
-
-  time.timeZone = "Europe/London";
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "uk";
-  };
-
-  programs.fuse.userAllowOther = true;
-
-  nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      warn-dirty = false;
-      trusted-users = [ "root" "@wheel" ];
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
     };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
+    networking = {
+      useDHCP = nixpkgs.lib.mkDefault true;
+      firewall.enable = true;
+      networkmanager.enable = true;
     };
+
+    fonts.packages = with pkgs;
+      [ (nerdfonts.override { fonts = [ "FiraCode" ]; }) ];
+
+    services.tailscale.enable = true;
+
+    time.timeZone = "Europe/London";
+    i18n.defaultLocale = "en_GB.UTF-8";
+
+    console = {
+      font = "Lat2-Terminus16";
+      keyMap = "uk";
+    };
+
+    programs.fuse.userAllowOther = true;
+
+    nix = {
+      settings = {
+        experimental-features = [ "nix-command" "flakes" ];
+        warn-dirty = false;
+        trusted-users = [ "root" "@wheel" ];
+      };
+
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 7d";
+      };
+    };
+
+    environment.systemPackages = with pkgs; [ git vim jq ];
+
+    systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+
+    nixpkgs.config.allowUnfree = true;
+    system.stateVersion = "23.05";
   };
-
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-    jq
-  ];
-
-  nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "23.05";
-};
 }
