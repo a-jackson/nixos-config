1979647142b7d4d3d07279156693b7b928464e4b
Andrew Jackson <andrew@a-jackson.co.uk>
2024-04-01 13:57:49 +0100

    deploy fixes

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 export NIX_SSHOPTS="-At"
 
-build_remote=false
-
 hosts="$1"
 shift
 
 if [ -z "$hosts" ]; then
-    sudo nixos-rebuild switch --flake .#$(hostname)
+    sudo nixos-rebuild switch --flake ".#$(hostname)"
 fi
 
 for host in ${hosts//,/ }; do
-    nixos-rebuild --flake .\#$host boot --target-host $host --use-remote-sudo --use-substitutes $@
+    nixos-rebuild --flake ".#$host" switch --target-host "root@$host" --build-host "root@$host" "$@"
 done
